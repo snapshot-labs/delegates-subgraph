c402bb3c2180ac906eaa7aea94ee98058ea92ae6
less <fabien@bonustrack.co>
2024-07-01 22:50:06 +0700

    chore: remove TokenHolder entity

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,85 +1,46 @@
-import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
-import { TokenHolder, Delegate, Governance } from '../generated/schema';
-
-export let ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts';
+import { Delegate, Governance } from '../generated/schema';
 
 export let BIGINT_ZERO = BigInt.fromI32(0);
 
 export let BIGDECIMAL_ZERO = new BigDecimal(BIGINT_ZERO);
 
-export let DEFAULT_DECIMALS = 18
+export let DEFAULT_DECIMALS = 18;
 
 export function toDecimal(value: BigInt, decimals: number = DEFAULT_DECIMALS): BigDecimal {
   let precision = BigInt.fromI32(10)
     .pow(<u8>decimals)
-    .toBigDecimal()
+    .toBigDecimal();
 
-  return value.divDecimal(precision)
+  return value.divDecimal(precision);
 }
 
-export function getOrCreateTokenHolder(id: Address, governanceId: Address): TokenHolder {
-  let entityId = `${governanceId.toHexString()}/${id.toHexString()}`
+export function getDelegate(id: Address, governanceId: Address): Delegate {
+  let entityId = `${governanceId.toHexString()}/${id.toHexString()}`;
 
-  let tokenHolder = TokenHolder.load(entityId)
-
-  if (tokenHolder == null) {
-    tokenHolder = new TokenHolder(entityId)
-    tokenHolder.governance = governanceId.toHexString()
-    tokenHolder.user = id
-    tokenHolder.tokenBalanceRaw = BIGINT_ZERO
-    tokenHolder.tokenBalance = BIGDECIMAL_ZERO
-    tokenHolder.totalTokensHeldRaw = BIGINT_ZERO
-    tokenHolder.totalTokensHeld = BIGDECIMAL_ZERO
-
-    if (id.toHexString() != ZERO_ADDRESS) {
-      let governance = getGovernanceEntity(governanceId)
-      governance.totalTokenHolders += 1
-      governance.save()
-    }
-
-    tokenHolder.save()
-  }
-
-  return tokenHolder as TokenHolder
-}
-
-export function getOrCreateDelegate(id: Address, governanceId: Address): Delegate {
-  let entityId = `${governanceId.toHexString()}/${id.toHexString()}`
-
-  let delegate = Delegate.load(entityId)
+  let delegate = Delegate.load(entityId);
 
   if (delegate == null) {
-    delegate = new Delegate(entityId)
-    delegate.governance = governanceId.toHexString()
-    delegate.user = id
-    delegate.delegatedVotesRaw = BIGINT_ZERO
-    delegate.delegatedVotes = BIGDECIMAL_ZERO
-    delegate.tokenHoldersRepresentedAmount = 0
-
-    if (id.toHexString() != ZERO_ADDRESS) {
-      let governance = getGovernanceEntity(governanceId)
-      governance.totalDelegates += 1
-      governance.save()
-    }
-
-    delegate.save()
+    delegate = new Delegate(entityId);
+    delegate.governance = governanceId.toHexString();
+    delegate.user = id;
+    delegate.delegatedVotesRaw = BIGINT_ZERO;
+    delegate.delegatedVotes = BIGDECIMAL_ZERO;
+    delegate.tokenHoldersRepresentedAmount = 0;
   }
 
-  return delegate as Delegate
+  return delegate;
 }
 
-export function getGovernanceEntity(governanceId: Address): Governance {
-  let governance = Governance.load(governanceId.toHexString())
+export function getGovernance(governanceId: Address): Governance {
+  let governance = Governance.load(governanceId.toHexString());
 
   if (governance == null) {
-    governance = new Governance(governanceId.toHexString())
-    governance.totalTokenHolders = 0
-    governance.currentTokenHolders = 0
-    governance.currentDelegates = 0
-    governance.totalDelegates = 0
-    governance.delegatedVotesRaw = BIGINT_ZERO
-    governance.delegatedVotes = BIGDECIMAL_ZERO
+    governance = new Governance(governanceId.toHexString());
+    governance.currentDelegates = 0;
+    governance.delegatedVotesRaw = BIGINT_ZERO;
+    governance.delegatedVotes = BIGDECIMAL_ZERO;
   }
 
-  return governance as Governance
+  return governance
 }
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,91 +1,39 @@
-import { dataSource } from '@graphprotocol/graph-ts'
-import { DelegateChanged, DelegateVotesChanged, Transfer } from '../generated/Token/Token'
-import {
-  toDecimal,
-  getOrCreateTokenHolder,
-  getOrCreateDelegate,
-  getGovernanceEntity,
-  ZERO_ADDRESS,
-  BIGINT_ZERO
-} from './helpers'
+import { dataSource } from '@graphprotocol/graph-ts';
+import { DelegateChanged, DelegateVotesChanged } from '../generated/Token/Token';
+import { toDecimal, getDelegate, getGovernance, BIGINT_ZERO } from './helpers';
 
 export function handleDelegateChanged(event: DelegateChanged): void {
-  let governanceId = dataSource.address()
+  let governanceId = dataSource.address();
 
-  let tokenHolder = getOrCreateTokenHolder(event.params.delegator, governanceId)
-  let previousDelegate = getOrCreateDelegate(event.params.fromDelegate, governanceId)
-  let newDelegate = getOrCreateDelegate(event.params.toDelegate, governanceId)
+  let previousDelegate = getDelegate(event.params.fromDelegate, governanceId);
+  previousDelegate.tokenHoldersRepresentedAmount -= 1;
+  previousDelegate.save();
 
-  tokenHolder.delegate = newDelegate.id
-  tokenHolder.save()
-
-  previousDelegate.tokenHoldersRepresentedAmount -= 1
-  previousDelegate.save()
-
-  newDelegate.tokenHoldersRepresentedAmount += 1
-  newDelegate.save()
+  let newDelegate = getDelegate(event.params.toDelegate, governanceId);
+  newDelegate.tokenHoldersRepresentedAmount += 1;
+  newDelegate.save();
 }
 
 export function handleDelegateVotesChanged(event: DelegateVotesChanged): void {
-  let governanceId = dataSource.address()
+  let governanceId = dataSource.address();
 
-  let governance = getGovernanceEntity(governanceId)
-  let delegate = getOrCreateDelegate(event.params.delegate, governanceId)
-  let votesDifference = event.params.newBalance.minus(event.params.previousBalance)
+  let governance = getGovernance(governanceId);
+  let delegate = getDelegate(event.params.delegate, governanceId);
+  let votesDifference = event.params.newBalance.minus(event.params.previousBalance);
 
-  delegate.delegatedVotesRaw = event.params.newBalance
-  delegate.delegatedVotes = toDecimal(event.params.newBalance)
-  delegate.save()
+  delegate.delegatedVotesRaw = event.params.newBalance;
+  delegate.delegatedVotes = toDecimal(event.params.newBalance);
+  delegate.save();
 
   if (event.params.previousBalance == BIGINT_ZERO && event.params.newBalance > BIGINT_ZERO) {
-    governance.currentDelegates += 1
+    governance.currentDelegates += 1;
   }
 
   if (event.params.newBalance == BIGINT_ZERO) {
-    governance.currentDelegates -= 1
+    governance.currentDelegates -= 1;
   }
 
-  governance.delegatedVotesRaw = governance.delegatedVotesRaw.plus(votesDifference)
-  governance.delegatedVotes = toDecimal(governance.delegatedVotesRaw)
-  governance.save()
-}
-
-export function handleTransfer(event: Transfer): void {
-  let governanceId = dataSource.address()
-
-  let fromHolder = getOrCreateTokenHolder(event.params.from, governanceId)
-  let toHolder = getOrCreateTokenHolder(event.params.to, governanceId)
-  let governance = getGovernanceEntity(governanceId)
-
-  if (event.params.from.toHexString() != ZERO_ADDRESS) {
-    let fromHolderPreviousBalance = fromHolder.tokenBalanceRaw
-    fromHolder.tokenBalanceRaw = fromHolder.tokenBalanceRaw.minus(event.params.amount)
-    fromHolder.tokenBalance = toDecimal(fromHolder.tokenBalanceRaw)
-
-    if (fromHolder.tokenBalanceRaw == BIGINT_ZERO && fromHolderPreviousBalance > BIGINT_ZERO) {
-      governance.currentTokenHolders -= 1
-      governance.save()
-    } else if (fromHolder.tokenBalanceRaw > BIGINT_ZERO && fromHolderPreviousBalance == BIGINT_ZERO) {
-      governance.currentTokenHolders += 1
-      governance.save()
-    }
-
-    fromHolder.save()
-  }
-
-  let toHolderPreviousBalance = toHolder.tokenBalanceRaw
-  toHolder.tokenBalanceRaw = toHolder.tokenBalanceRaw.plus(event.params.amount)
-  toHolder.tokenBalance = toDecimal(toHolder.tokenBalanceRaw)
-  toHolder.totalTokensHeldRaw = toHolder.totalTokensHeldRaw.plus(event.params.amount)
-  toHolder.totalTokensHeld = toDecimal(toHolder.totalTokensHeldRaw)
-
-  if (toHolder.tokenBalanceRaw == BIGINT_ZERO && toHolderPreviousBalance > BIGINT_ZERO) {
-    governance.currentTokenHolders -= 1
-    governance.save()
-  } else if (toHolder.tokenBalanceRaw > BIGINT_ZERO && toHolderPreviousBalance == BIGINT_ZERO) {
-    governance.currentTokenHolders += 1
-    governance.save()
-  }
-
-  toHolder.save()
+  governance.delegatedVotesRaw = governance.delegatedVotesRaw.plus(votesDifference);
+  governance.delegatedVotes = toDecimal(governance.delegatedVotesRaw);
+  governance.save();
 }
